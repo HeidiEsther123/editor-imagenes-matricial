75def7d725efb48129235eaf0f4f824837984da3
HeidiEsther123 <heidi.pena@tecdesoftware.edu.mx>
2025-11-16 12:21:34 -0600

    Ejercicio 4.1 Resuelto

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -485,19 +485,26 @@ function rotar90Grados(matriz) {
 function mezclarImagenes(matriz1, matriz2, factor) {
   // TODO: Implementar mezcla de imágenes
   
-  // 1. Verificar que tengan las mismas dimensiones
-  // const dims1 = obtenerDimensiones(matriz1);
-  // const dims2 = obtenerDimensiones(matriz2);
-  // if (dims1.filas !== dims2.filas || dims1.columnas !== dims2.columnas) {
-  //   throw new Error('Las imágenes deben tener el mismo tamaño');
-  // }
-  
-  // 2. Para cada pixel:
-  // r = r1 * (1 - factor) + r2 * factor
-  // g = g1 * (1 - factor) + g2 * factor
-  // b = b1 * (1 - factor) + b2 * factor
-  
-  return []; // REEMPLAZAR
+  const filas = matriz1.length;
+  const columnas = matriz1[0].length;
+  const resultado = [];
+
+  for (let i = 0; i < filas; i++) {
+    const fila = [];
+    for (let j = 0; j < columnas; j++) {
+      const p1 = matriz1[i][j];
+      const p2 = matriz2[i][j];
+
+      fila.push({
+        r: Math.round(p1.r * (1 - factor) + p2.r * factor),
+        g: Math.round(p1.g * (1 - factor) + p2.g * factor),
+        b: Math.round(p1.b * (1 - factor) + p2.b * factor),
+        a: Math.round(p1.a * (1 - factor) + p2.a * factor),
+      });
+    }    
+    resultado.push(fila);
+  }
+  return resultado; // REEMPLAZAR
 }
 
 /**
